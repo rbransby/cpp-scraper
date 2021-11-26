ec61484dab4b1b10992075d24d6af861a07bfe25
Rob Bransby <rob@bransby.com.au>
2021-11-26 22:32:45 +0800

    all deploys

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import axios from "axios";
-import { ParkingLot } from "../../types/ParkingLot";
+import {ParkingLot} from "../../types/ParkingLot";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -14,26 +14,26 @@ import { ParkingLot } from "../../types/ParkingLot";
 admin.initializeApp();
 
 export const scrapeData = async () => {
-    const url = `https://www.cityofperthparking.com.au/json/cpp/map/carpark/0?${Math.floor(
-        Date.now()
-    )}`;
-    const { data } = await axios.get<ParkingLot[]>(url);
-    return data;
+  const url = `https://www.cityofperthparking.com.au/json/cpp/map/carpark/0?${Math.floor(
+      Date.now()
+  )}`;
+  const {data} = await axios.get<ParkingLot[]>(url);
+  return data;
 };
 
-export const scheduledCPPScrape = functions.https.onRequest(
-    async (req, res) => {
-        const parkingSpaceData = await scrapeData();
-        await admin
-            .firestore()
-            .collection("liveParkingSpaceData")
-            .doc("liveParkingSpaceData")
-            .set({data:parkingSpaceData});
-        await admin
-            .firestore()
-            .collection("timeSeriesParkingSpaces")
-            .doc(`${Math.floor(Date.now() / 1000)}`)
-            .set({data:parkingSpaceData});
-        return;
-    }
-);
+export const scheduledCPPScrape = functions.pubsub
+    .schedule("every 15 minutes")
+    .onRun(async (context) => {
+      const parkingSpaceData = await scrapeData();
+      await admin
+          .firestore()
+          .collection("liveParkingSpaceData")
+          .doc("liveParkingSpaceData")
+          .set({data: parkingSpaceData});
+      await admin
+          .firestore()
+          .collection("timeSeriesParkingSpaces")
+          .doc(`${Math.floor(Date.now() / 1000)}`)
+          .set({data: parkingSpaceData});
+      return;
+    });
