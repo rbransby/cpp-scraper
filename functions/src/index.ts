51d10a6e2e0fa9c569f7e8a7ef121165f49d944b
Rob Bransby <rob@bransby.com.au>
2021-11-28 19:49:46 +0800

    adjusting eslint config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,69 +1,68 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import axios from "axios";
-import { ParkingLot } from "../../types/ParkingLot";
+import * as functions from "firebase-functions"
+import * as admin from "firebase-admin"
+import axios from "axios"
+import { ParkingLot } from "../../types/ParkingLot"
 import firestore = require("@google-cloud/firestore");
 
-admin.initializeApp();
+admin.initializeApp()
 
 export const scrapeData = async () => {
-    const url = `https://www.cityofperthparking.com.au/json/cpp/map/carpark/0?${Math.floor(
+  const url = `https://www.cityofperthparking.com.au/json/cpp/map/carpark/0?${Math.floor(
         Date.now()
-    )}`;
-    const { data } = await axios.get<ParkingLot[]>(url);
-    return data;
-};
+    )}`
+  const { data } = await axios.get<ParkingLot[]>(url)
+  return data
+}
 
 export const scheduledCPPScrape = functions.pubsub
-    .schedule("every 15 minutes")
-    .onRun(async (context) => {
-        const timestampValue = Math.floor(Date.now() / 1000);
-        const parkingSpaceData = await scrapeData();
-        await admin
-            .firestore()
-            .collection("liveParkingSpaceData")
-            .doc("liveParkingSpaceData")
-            .set({ data: parkingSpaceData });
-        for (const parkingLot of parkingSpaceData) {
-            await admin
-                .firestore()
-                .collection(`previousParkingSpaceData_${parkingLot.nid}`)
-                .doc(`${timestampValue}`)
-                .set({ timestamp: timestampValue, ...parkingLot });
-        }
-        return;
-    });
+  .schedule("every 15 minutes")
+  .onRun(async () => {
+    const timestampValue = Math.floor(Date.now() / 1000)
+    const parkingSpaceData = await scrapeData()
+    await admin
+      .firestore()
+      .collection("liveParkingSpaceData")
+      .doc("liveParkingSpaceData")
+      .set({ data: parkingSpaceData })
+    for (const parkingLot of parkingSpaceData) {
+      await admin
+        .firestore()
+        .collection(`previousParkingSpaceData_${parkingLot.nid}`)
+        .doc(`${timestampValue}`)
+        .set({ timestamp: timestampValue, ...parkingLot })
+    }
+  })
 
 export const exportDatabase = functions.pubsub
-    .schedule("every 1 hour")
-    .onRun((context) => {
-        // Client
-        const client = new firestore.v1.FirestoreAdminClient();
+  .schedule("every 1 hours")
+  .onRun(() => {
+    // Client
+    const client = new firestore.v1.FirestoreAdminClient()
 
-        // Get project ID
-        const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-        functions.logger.log("Backing up project: ", projectId);
+    // Get project ID
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT
+    functions.logger.log("Backing up project: ", projectId)
 
-        // Bucket name - has to start with gs://
-        let bucket = "gs://BUCKET_NAME_DEVELOPMENT";
+    // Bucket name - has to start with gs://
+    const bucket = "gs://parkingspacetracker-ac214.appspot.com"
 
-        if (!projectId) throw new Error("No projectId found");
+    if (!projectId) throw new Error("No projectId found")
 
-        // Get database name
-        const databaseName = client.databasePath(projectId, "(default)");
+    // Get database name
+    const databaseName = client.databasePath(projectId, "(default)")
 
-        return client
-            .exportDocuments({
-                name: databaseName,
-                outputUriPrefix: bucket,
-                collectionIds: []
-            })
-            .then((responses: any) => {
-                const response = responses[0];
-                console.log(`Operation Name: ${response["name"]}`);
-            })
-            .catch((err: Error) => {
-                console.error(err);
-                throw new Error("Export operation failed");
-            });
-    });
+    return client
+      .exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        collectionIds: []
+      })
+      .then((responses: any) => {
+        const response = responses[0]
+        console.log(`Operation Name: ${response.name}`)
+      })
+      .catch((err: Error) => {
+        console.error(err)
+        throw new Error("Export operation failed")
+      })
+  })
