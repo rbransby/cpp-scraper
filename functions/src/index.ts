e22c237ce2762023f92198e1d744332dba017c5b
Rob Bransby <rob@bransby.com.au>
2021-11-28 09:48:16 +0800

    adding export function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,14 +2,8 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import axios from "axios";
 import { ParkingLot } from "../../types/ParkingLot";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import firestore = require('@google-cloud/firestore');
+import { EventContext } from 'firebase-functions'
 
 admin.initializeApp();
 
@@ -37,3 +31,38 @@ export const scheduledCPPScrape = functions.https.onRequest(
         return;
     }
 );
+
+export const exportDatabase = functions.pubsub
+                                .schedule('every 1 hour')
+                                .onRun((context) => {
+  
+  // Client
+  const client = new firestore.v1.FirestoreAdminClient()
+
+  // Get project ID
+  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT
+  functions.logger.log('Backing up project: ', projectId)
+
+  // Bucket name - has to start with gs://
+  let bucket = "gs://BUCKET_NAME_DEVELOPMENT"
+  
+  if (!projectId)
+    throw new Error("No projectId found");
+
+  // Get database name
+  const databaseName = client.databasePath(projectId, '(default)')
+
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+    collectionIds: []
+  })
+    .then((responses : any) => {
+      const response = responses[0]
+      console.log(`Operation Name: ${response['name']}`)
+    })
+    .catch((err : Error) => {
+      console.error(err)
+      throw new Error('Export operation failed')
+    })
+});
