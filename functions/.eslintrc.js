e482ca4f392b9ee17288954af37d9fdd99803db9
Rob Bransby <rob@bransby.com.au>
2021-11-29 08:03:03 +0800

    linting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "standard"
-  ],
+	env:{node:true},
+  extends: ["eslint:recommended"],
   rules: {
     quotes: ["error", "double"],
     "no-console": "off",
     "no-empty": 0,
-    "no-unused-vars": ["error", { args: "after-used" }]
-  }
-}
+    "no-unused-vars": ["error", { args: "after-used" }],
+  },
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,55 +1,56 @@
-import * as functions from "firebase-functions"
-import * as admin from "firebase-admin"
-import axios from "axios"
-import { ParkingLot } from "../../types/ParkingLot"
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import axios from "axios";
+import { ParkingLot } from "../../types/ParkingLot";
 import firestore = require("@google-cloud/firestore");
 
-admin.initializeApp()
+admin.initializeApp();
 
 export const scrapeData = async () => {
   const url = `https://www.cityofperthparking.com.au/json/cpp/map/carpark/0?${Math.floor(
-        Date.now()
-    )}`
-  const { data } = await axios.get<ParkingLot[]>(url)
-  return data
-}
+    Date.now()
+  )}`;
+  const { data } = await axios.get<ParkingLot[]>(url);
+  return data;
+};
 
 export const scheduledCPPScrape = functions.pubsub
   .schedule("every 15 minutes")
   .onRun(async () => {
-    const timestampValue = Math.floor(Date.now() / 1000)
-    const parkingSpaceData = await scrapeData()
+    const timestampValue = Math.floor(Date.now() / 1000);
+    const parkingSpaceData = await scrapeData();
     await admin
       .firestore()
       .collection("liveParkingSpaceData")
       .doc("liveParkingSpaceData")
-      .set({ data: parkingSpaceData })
+      .set({ data: parkingSpaceData });
     for (const parkingLot of parkingSpaceData) {
       await admin
         .firestore()
         .collection(`previousParkingSpaceData_${parkingLot.nid}`)
         .doc(`${timestampValue}`)
-        .set({ timestamp: timestampValue, ...parkingLot })
+        .set({ timestamp: timestampValue, ...parkingLot });
     }
-  })
+  });
 
 export const exportDatabase = functions.pubsub
   .schedule("every 1 hours")
   .onRun(() => {
     // Client
-    const client = new firestore.v1.FirestoreAdminClient()
+    const client = new firestore.v1.FirestoreAdminClient();
 
     // Get project ID
-    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT
-    functions.logger.log("Backing up project: ", projectId)
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+    functions.logger.log("Backing up project: ", projectId);
 
     // Bucket name - has to start with gs://
-    const bucket = "gs://parkingspacetracker-ac214.appspot.com"
+    const bucket =
+      "gs://parkingspacetracker-ac214.appspot.com/ParkingSpaceExport";
 
-    if (!projectId) throw new Error("No projectId found")
+    if (!projectId) throw new Error("No projectId found");
 
     // Get database name
-    const databaseName = client.databasePath(projectId, "(default)")
+    const databaseName = client.databasePath(projectId, "(default)");
 
     return client
       .exportDocuments({
@@ -58,11 +59,11 @@ export const exportDatabase = functions.pubsub
         collectionIds: []
       })
       .then((responses: any) => {
-        const response = responses[0]
-        console.log(`Operation Name: ${response.name}`)
+        const response = responses[0];
+        console.log(`Operation Name: ${response.name}`);
       })
       .catch((err: Error) => {
-        console.error(err)
-        throw new Error("Export operation failed")
-      })
-  })
+        console.error(err);
+        throw new Error("Export operation failed");
+      });
+  });
